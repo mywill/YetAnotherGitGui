129ea842e3eac1d5846efca5cd54d81dbda180bb
Mywill <kennethdillon@protonmail.com>
2026-05-03 11:28:51 -0400

    chore: fixes for ts update and remove dependabot yml

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,14 @@ export default defineConfig({
     port: 1420,
     strictPort: true,
     watch: {
-      ignored: ["**/src-tauri/**"],
+      ignored: [
+        "**/src-tauri/**",
+        "**/.pnpm-store/**",
+        "**/coverage/**",
+        "**/playwright-report/**",
+        "**/test-results/**",
+        "**/dist/**",
+      ],
     },
   },
 });
